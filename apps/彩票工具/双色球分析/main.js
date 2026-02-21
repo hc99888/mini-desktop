da20c324a97908a682bb6647d5803a937904bfc8
hc99888 <131562483+hc99888@users.noreply.github.com>
2026-02-21 15:08:19 +0800

    Delete apps/彩票工具 directory

--- apps/彩票工具/双色球分析/main.js
+++
@@ -1,9 +0,0 @@
-const note = document.getElementById("note");
-
-// 加载本地内容
-note.value = localStorage.getItem("note-content") || "";
-
-// 自动保存
-note.addEventListener("input", () => {
-  localStorage.setItem("note-content", note.value);
-});
